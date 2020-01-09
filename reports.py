646e7d58a7fbb4dc4b3127a3f5cbdd012c1c426d
Daniel Lew <43003692+dqniellew1@users.noreply.github.com>
2020-01-08 22:04:44 -0600

    Update reports.py

--- reports.py
+++ reports.py
@@ -2,7 +2,6 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
-import matplotlib.pyplot as plt
 import time
 import streamlit as st
 
@@ -39,54 +38,80 @@ def micro_view(data):
         st.subheader("Below is the seasonal grade for %s: for all year" % selected_lake)
         #year = st.slider("Selected year",min_value=2004, max_value=2014, step=1)
         #if year:
-    grade_df = new_df[['seasonal.grade']]
-    replace_nums = {"A": 5, "B": 4,"C":3,"D":2,"E":1,"F":0}
-    grade_df["num_grade"] = grade_df["seasonal.grade"].replace(replace_nums)
-    grade_df['Y'] = grade_df.apply(lambda x: 0.5, axis=1)
-    layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)',
-        plot_bgcolor='rgba(0,0,0,0)',width = 900,height = 400,
-        yaxis = dict(range = [0,1.5],showticklabels=False,showgrid=False),xaxis = dict(
-        tickmode = 'linear',
-        tick0 = 2003,
-        dtick = 1,showgrid=False
-    ))
+        grade_df = new_df[['seasonal.grade']]
+        replace_nums = {"A": 5, "B": 4,"C":3,"D":2,"E":1,"F":0}
+        grade_df["num_grade"] = grade_df["seasonal.grade"].replace(replace_nums)
+        grade_df['Y'] = grade_df.apply(lambda x: 0.5, axis=1)
+        layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)',
+                           plot_bgcolor='rgba(0,0,0,0)',
+                           width = 900,
+                           height = 300,
+                           yaxis = dict(range = [0,1],
+                                        showticklabels=False,
+                                        showgrid=False),
+                           xaxis = dict(tickmode = 'linear',
+                                        tick0 = grade_df.index.min(),
+                                        dtick = 1,
+                                        showgrid=False
+                                       ))
 
+        colorsIdx = {'A': 'rgb(17,84,23)',
+                     'B': 'rgb(21,159,34)',
+                     'C': 'rgb(143,179,24)',
+                     'D': 'rgb(238,144,21)',
+                     'E': 'rgb(238,79,21)',
+                     'F': 'rgb(255,44,44)'}
+        textIdx = {'A':'A',
+                   'B':'B',
+                   'C':'C',
+                   'D':'D',
+                   'E':'E',
+                   'F':'F'}
 
-    colorsIdx = {'A': 'rgb(17,84,23)', 'B': 'rgb(21,159,34)','F':'rgb(255,44,44)','C':'rgb(143,179,24)',
-        'D':'rgb(238,144,21)','E':'rgb(238,79,21)'}
-    textIdx = {'A':'A','B':'B','C':'C','D':'D','E':'E','F':'F'}
-    cols = grade_df['seasonal.grade'].map(colorsIdx)
-    texts = grade_df['seasonal.grade'].map(textIdx)
+        cols = grade_df['seasonal.grade'].map(colorsIdx)
+        texts = grade_df['seasonal.grade'].map(textIdx)
 
-        
+        fig2 = go.Figure(layout = layout)
+        fig2.add_trace(
+            go.Scatter(mode='markers+text',
+                       x=grade_df.index,
+                       y=grade_df.Y,
+                       hovertemplate = "Year: %{x}",
 
-    fig2 = go.Figure(layout = layout)
-    fig2.add_trace(
-            go.Scatter(
-            mode='markers+text',
-            x=grade_df.index,
-            y=grade_df.Y,
-            hoverinfo='skip',
-            marker=dict(
-            size=45,
-            color = cols),
-            showlegend=False,
-            text = texts,
-            textposition="bottom center",
-            textfont=dict(
-                family="sans serif",
-                size=23,
-                color="purple"
-    )
-        )
+                       marker=dict(
+                           size=45,
+                           color = cols),
+                       showlegend=False,
+                       text = texts,
+                       textposition="bottom center",
+                       textfont=dict(family="sans serif",
+                                     size=23,
+                                     color="purple")))
+        st.plotly_chart(fig2)
+        # st.text("The  seasonal grade for %s is %s" % (selected_lake, grade_df['seasonal.grade'].values))
 
-    )
-
-
-
-        
-
-    st.plotly_chart(fig2)
+        # Plot map
+        new_df1 = new_df[['latitude', 'longitude']]
+        st.subheader(' Geographic data at %s 🌊'% selected_lake)
+        st.deck_gl_chart(
+                viewport={
+                    'latitude': 44.9799700,
+                    'longitude': -93.2638400,
+                    'zoom': 8,
+                    'pitch': 50,
+                 },
+                 layers=[{
+                    'type':'PointCloudLayer',
+                    'data': new_df1,
+                    'radius': 1500,
+                    'elevationScale': 80,
+                    'elevationRange': [800, 10000],
+                    'pickable': False,
+                    'extruded': False,
+             },     {
+                     'type': 'ScatterplotLayer',
+                     'data': new_df1,
+                     }])
 
 def macro_view():
     st.subheader("Average number of properties around major watersheds")
@@ -208,43 +233,7 @@ def macro_view():
                     ))
     st.plotly_chart(fig)
 
-    #fig3 = make_subplots()
-      
-        
-        
-
-        
-
-
-    # st.text("The  seasonal grade for %s is %s" % (selected_lake, grade_df['seasonal.grade'].values))
-
-    # Plot map
-    new_df1 = new_df[['latitude', 'longitude']]
-    st.subheader(' Geographic data at %s 🌊'% selected_lake)
-    st.deck_gl_chart(
-            viewport={
-                'latitude': 44.9799700,
-                'longitude': -93.2638400,
-                'zoom': 8,
-                'pitch': 50,
-             },
-             layers=[{
-                'type':'PointCloudLayer',
-                'data': new_df1,
-                'radius': 1500,
-                'elevationScale': 80,
-                'elevationRange': [800, 10000],
-                'pickable': False,
-                'extruded': False,
-         },     {
-                 'type': 'ScatterplotLayer',
-                 'data': new_df1,
-                 }])
-
-
-
-        
-    selected_ws = st.selectbox('Select the lake you would like to examine:', data["MAJOR_WATERSHED_y"].unique())
+    selected_ws = st.selectbox('Select the watershed you would like to examine:', data["MAJOR_WATERSHED_y"].unique())
     st.subheader("Below are the lakes in {} watershed for each year".format(selected_ws))
 
         # if selected_ws:
@@ -255,20 +244,24 @@ def macro_view():
         #     color=new_dfws.index, hover_name="LAKE_NAME_x",size_max=10,
         #     range_x=[0,5.5], range_y=[0,5])
         #     fig3.update_layout(transition = {'duration': 4000})
-            
-
         # st.plotly_chart(fig3)
 
     if selected_ws:
         new_dfws = data[data["MAJOR_WATERSHED_y"] == selected_ws].set_index("seasonal.grade")
         new_dfws = new_dfws.sort_index(axis=0)
-        fig_scatter = px.scatter(new_dfws,y=new_dfws.index,opacity=0.5,
-                                        x = "Number Properties",color =new_dfws.index ,
-                                            animation_frame="Year",animation_group="LAKE_NAME_x",
-                                            facet_col="COUNTY",hover_name="LAKE_NAME_x",facet_col_wrap=2 )
+        fig_scatter = px.scatter(new_dfws,y=new_dfws.index,
+        opacity=0.5,
+        x = "Number Properties",
+        color =new_dfws.index ,
+        animation_frame="Year",
+        animation_group="LAKE_NAME_x",
+        facet_col="COUNTY",
+        hover_name="LAKE_NAME_x",
+        facet_col_wrap=2)
+        
         fig_scatter.update_traces(marker=dict(size=14,
-                              line=dict(width=2,
-                                        color='DarkSlateGrey')),
+        line=dict(width=2,
+        color='DarkSlateGrey')),
                   selector=dict(mode='markers'))
         fig_scatter.update_layout(transition = {'duration': 8000})
         fig_scatter.update_xaxes(title_text='')
@@ -280,21 +273,8 @@ def macro_view():
         family="Courier New, monospace",
         size=14,
         color="#7f7f7f"
-    )
-)
-
+    ))
     st.plotly_chart(fig_scatter)
-
-
-
-
-
-
-
-
-
-
-
 
     st.write("# Properties Report 🏣")
     st.subheader("Median Price of Properties at %s" % selected_lake)
